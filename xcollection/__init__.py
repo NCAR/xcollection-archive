4ca4cbcbd8d0cd1de6b7a6f3d7d48d656e101483
Julia Kent <jkent@ucar.edu>
2019-02-01 11:10:00 -0700

    Changed _init_.py

--- xcollection/__init__.py
+++ xcollection/__init__.py
@@ -1,4 +1,6 @@
-import importlib
+"""Top-level package for xcollection."""
 
-from .core import datasource
-from .core import analyzed_datasource
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
