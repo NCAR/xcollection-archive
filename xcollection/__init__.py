485cc7f2bd77819d9b38c06642e1062a5d6dd091
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-02-26 13:40:04 -0700

    Fix isort issues

--- xcollection/__init__.py
+++ xcollection/__init__.py
@@ -1,9 +1,7 @@
 """Top-level package for xcollection."""
-from .core import analyzed_collection, operator
-from .config import set_options, get_options
-
-
 from ._version import get_versions
+from .config import get_options, set_options
+from .core import analyzed_collection, operator
 
 __version__ = get_versions()['version']
 del get_versions
