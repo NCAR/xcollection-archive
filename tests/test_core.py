545e225027e01c0c2b254c2681b73a6266f99f3c
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-02-26 13:19:58 -0700

    Fix flake8 issues

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,28 +1,35 @@
-import yaml
 import xarray as xr
-
+import yaml
 from esmlab.climatology import compute_mon_climatology
+
 from xcollection import analyzed_collection, operator
 
 
 def test_analyzed_collection():
 
-    recipe = {'name': 'monclim',
-              'description': 'compute monthly climatology',
-              'operators': [
-                  operator(applied_method="time:mon_clim",
-                           module='esmlab.climatology',
-                           function='compute_mon_climatology'),
-                  ]}
+    recipe = {
+        'name': 'monclim',
+        'description': 'compute monthly climatology',
+        'operators': [
+            operator(
+                applied_method='time:mon_clim',
+                module='esmlab.climatology',
+                function='compute_mon_climatology',
+            )
+        ],
+    }
 
     query = dict(case='cesm_pop_sample', variable='NO3')
 
-    dc = analyzed_collection(collection='test_collection',
-                             analysis_recipe=recipe,
-                             overwrite_existing=True,
-                             file_format="nc",
-                             **query)
+    dc = analyzed_collection(
+        collection='test_collection',
+        analysis_recipe=recipe,
+        overwrite_existing=True,
+        file_format='nc',
+        **query
+    )
     assert isinstance(dc.to_xarray(), xr.Dataset)
+
 
 def test_read_recipe():
     with open('tests/data/recipes.yml') as fid:
--- xcollection/__init__.py
+++ xcollection/__init__.py
@@ -1,9 +1,9 @@
 """Top-level package for xcollection."""
+from .core import analyzed_collection, operator
+from .config import set_options, get_options
+
 
 from ._version import get_versions
 
-__version__ = get_versions()["version"]
+__version__ = get_versions()['version']
 del get_versions
-
-from .config import set_options, get_options
-from .core import analyzed_collection, operator
