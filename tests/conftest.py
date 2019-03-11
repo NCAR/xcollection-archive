284bba8125623ccc939534853b30d2715c3f9aca
mclong <mclong@ucar.edu>
2019-03-11 12:44:34 -0600

    fixed test: update intake_esm.config

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,6 @@ import esmlab
 import intake
 import intake_esm
 
-intake_esm.set_options(database_directory='tests/data')
+intake_esm.config.set(dict(database_directory='tests/data'))
 
 col = intake.open_esm_metadatastore(collection_input_file='tests/data/collection_input.yml')
